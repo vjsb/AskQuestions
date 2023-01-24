0d42e288803d5974103694add063dd8330098117
João Victor Cariolano <joao.victorcoriolano@gmail.com>
2023-01-24 00:44:32 -0300

    aplicando conceitos de variaveis do EJS

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 //Dizendo para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-    res.render("principal/perfil.ejs")
+app.get("/:nome/:lang", function(req, res){
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Ask Company",
+        localizacao: "São Paulo"
+    }) 
 })
 
 app.listen(8080, ()=> {console.log("App executado com sucesso!")})
