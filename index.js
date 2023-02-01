2903e1d372f0184c45a3d01ad1ec58315cbd1b2c
João Victor Cariolano <joao.victorcoriolano@gmail.com>
2023-02-01 19:54:05 -0300

    add arquivos estaticos e logo do projeto

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 //Dizendo para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get("/:nome/:lang", function(req, res){
     var nome = req.params.nome;
