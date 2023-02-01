aa6077d4fe45343a4075e2729661dd4e1999ab2f
João Victor Cariolano <joao.victorcoriolano@gmail.com>
2023-02-01 20:34:35 -0300

    removendo arquivos ejs e instalando bootstrap no projeto

--- index.js
+++ index.js
@@ -5,27 +5,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.get("/:nome/:lang", function(req, res){
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    exibirMsg = true;
+app.get("/perguntar", function(req, res){
+    res.render("perguntar")
+})
 
-    var produtos = [
-        {nome: "Coca-Cola", preco: 4.5},
-        {nome: "Doritos", preco: 3},
-        {nome: "Leite", preco: 4.3},
-        {nome: "Nescau", preco: 5},
-        {nome: "Açucar", preco: 7.3},
-    ]
+app.get("/", function(req, res){
 
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Ask Company",
-        localizacao: "São Paulo",
-        msg: exibirMsg,
-        produtos: produtos
-    }) 
+    res.render("index") 
 })
 
 app.listen(8080, ()=> {console.log("App executado com sucesso!")})
